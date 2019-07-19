5d3df1a7578d516c813ff11d4855a8dad6050125
Anders Pitman <tapitman11@gmail.com>
2019-07-19 10:17:19 -0600

    Implement basic entry loading

--- components.js
+++ components.js
@@ -1,18 +1,57 @@
-const Main = () => {
+const Main = (entries) => {
   const dom = document.createElement('div');
 
   dom.classList.add('main');
+
   dom.appendChild(Header());
+  dom.appendChild(Feed(entries));
+
+  return dom;
+};
+
+const Feed = (entries) => {
+
+  const dom = document.createElement('div');
+  dom.classList.add('feed');
+
+  const list = document.createElement('div');
+  list.classList.add('entry-list');
+
+  entries.forEach((e) => {
+    const entry = document.createElement('div');
+    entry.classList.add('entry-list__entry');
+    entry.appendChild(Entry(e));
+    list.appendChild(entry);
+  });
+  dom.appendChild(list);
 
   return dom;
 };
 
 
+const Entry = (entry) => {
+  const dom = document.createElement('div');
+  dom.classList.add('entry');
+
+  if (entry.metadata.format === 'html') {
+    const path = entry.rootDir + '/' + entry.name;
+    dom.innerHTML = entry.content.replace("src='", `src='${path}/`);
+  }
+  else {
+    throw new Error("invalid format");
+  }
+  
+  return dom;
+};
+
 const Header = () => {
   const h1 = document.createElement('h1');
-  h1.innerHTML = "Hi there";
+  h1.innerHTML = "Feeds";
   return h1;
 };
 
 
-export { Main };
+export {
+  Main,
+  Feed,
+};
--- index.js
+++ index.js
@@ -1,14 +1,64 @@
+import { ClientBuilder } from '/client/dist/bundle.esm.js';
 import { Main } from './components.js';
 
 
 (async () => {
 
-  const main = Main();
+  const key = document.cookie.split('=')[1];
+
+  const rootDir = '/entries';
+
+  const result = await fetch(rootDir);
+  const tree = await result.json();
+
+  console.log(tree);
+
+  // sort in reverse-chronological order
+  let sortedNames = Object.keys(tree.children)
+    .sort()
+    .reverse();
+
+  console.log(sortedNames);
+
+  const entries = [];
+
+  for (const name of sortedNames) {
+
+    const entry = {
+      name,
+      rootDir,
+    };
+
+    let result = await fetch(rootDir + '/' + name + '/metadata.json');
+    entry.metadata = await result.json();
+
+    result = await fetch(rootDir + '/' + name + '/' + entry.metadata.contentFilename);
+    entry.content = await result.text();
+
+    entries.push(entry);
+  }
+
+  console.log(entries);
+
+
+
+
+  const main = Main(entries);
   const root = document.getElementById('root');
   root.appendChild(main);
 
-  const response = await fetch('/');
-  const text = await response.text();
-  console.log(text);
+  //const client = await new ClientBuilder()
+  //  .authKey(key)
+  //  .port(9001)
+  //  .secure(false)
+  //  .build();
+
+  //const metaStream = await client.getMetaStream('/');
+
+  //metaStream.onData((data) => {
+  //  console.log(data);
+  //});
+
+  //metaStream.request(100);
 
 })();
