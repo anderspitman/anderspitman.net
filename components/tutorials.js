b366086dc8ace19da9cc24836d6fa65f94caef9b
Anders Pitman <tapitman11@gmail.com>
2019-08-27 08:47:28 -0600

    Make tutorial names bold

--- components/tutorials.js
+++ components/tutorials.js
@@ -1,17 +1,13 @@
 const Tutorials = (tutList) => {
   const dom = document.createElement('div');
 
-  dom.innerHTML = `
-    <h1>Tuts</h1>
-  `;
-
   const ul = document.createElement('ul');
 
   for (const tut of tutList) {
     const li = document.createElement('li');
     const a = document.createElement('a');
     a.setAttribute('href', tut.path);
-    a.textContent = tut.name + " | " + tut.metadata.name;
+    a.innerHTML = tut.name + " | <strong>" + tut.metadata.name + "</strong>";
 
     a.addEventListener('click', (e) => {
       e.preventDefault();
